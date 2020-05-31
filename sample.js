51943aa577cc780c8df9d0a4d7ba30adb6413ea3
HayatoFunahashi <hayato_funahashi@icloud.com>
2020-05-31 15:07:39 +0900

    alias apikey

--- sample.js
+++ sample.js
@@ -3,5 +3,6 @@ function initMap() {
     zoom: 15,
     center: new google.maps.LatLng(35.1253694,136.9073667)
   };
+  var APIKEY = AIzaSyCc_UDQf7wKx_5WkwRIYKKUnnwjcI2XTLM
   var map = new google.maps.Map(document.getElementById("map"), opts);
 }
